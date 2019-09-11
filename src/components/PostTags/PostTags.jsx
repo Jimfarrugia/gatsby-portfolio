6384cb153309f8ceb4414b3071e392be4204b84b
Jim <the.jim.farrugia@gmail.com>
2019-09-11 21:49:47 +1000

    PostTags styling

--- src/components/PostTags/PostTags.jsx
+++ src/components/PostTags/PostTags.jsx
@@ -2,21 +2,27 @@ import React, { Component } from "react";
 import _ from "lodash";
 import { Link } from "gatsby";
 
+import "./PostTags.scss";
+
 class PostTags extends Component {
   render() {
     const { tags } = this.props;
     return (
       <div className="post-tag-container">
-        {tags &&
-          tags.map(tag => (
+        {tags && (
+        <>
+          <h5>Tags:</h5>
+          {tags.map(tag => (
             <Link
               key={tag}
               style={{ textDecoration: "none" }}
               to={`/tags/${_.kebabCase(tag)}`}
             >
-              <button>{tag}</button>
+              <button type="button" className="btn">{tag}</button>
             </Link>
           ))}
+        </>
+        )}
       </div>
     );
   }
