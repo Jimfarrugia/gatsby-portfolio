e1c459787a05ca3241d212e2822fc7fa3d741ec9
Jim <the.jim.farrugia@gmail.com>
2019-09-30 12:08:02 +1000

    gatsby-plugin-favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,6 +105,37 @@ module.exports = {
         ]
       }
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+  
+        // WebApp Manifest Configuration
+        appName: null, // Inferred with your package.json
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-US',
+        background: '#003f8f',
+        theme_color: '#fff21a',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/?homescreen=1',
+        version: '1.0',
+  
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
     "gatsby-plugin-offline",
     {
       resolve: "gatsby-plugin-feed",
