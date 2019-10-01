711065f524f5645fe47e27e65faa144911a2795b
Jim <the.jim.farrugia@gmail.com>
2019-10-02 09:25:51 +1000

    footer links open in new tab

--- src/components/IconMenu/IconMenu.jsx
+++ src/components/IconMenu/IconMenu.jsx
@@ -13,25 +13,25 @@ import "./IconMenu.scss";
 
 const IconMenu = ({ config: { userLinks }, iconSize }) => (
   <div className="icon-menu center">
-    <a href={userLinks.github} title="Github">
+    <a href={userLinks.github} title="Github" target="_blank" rel="noopener noreferrer">
       <Icon path={mdiGithubFace} size={iconSize} />
     </a>
-    <a href={userLinks.medium} style={{marginBottom: -2}} title="Medium">
+    <a href={userLinks.medium} style={{marginBottom: -2}} title="Medium" target="_blank" rel="noopener noreferrer">
       <Icon path={mdiMedium} size={iconSize} />
     </a>
-    <a href={userLinks.linkedin} title="LinkedIn">
+    <a href={userLinks.linkedin} title="LinkedIn" target="_blank" rel="noopener noreferrer">
       <Icon path={mdiLinkedin} size={iconSize} />
     </a>
-    <a href={userLinks.twitter} title="Twitter">
+    <a href={userLinks.twitter} title="Twitter" target="_blank" rel="noopener noreferrer">
       <Icon path={mdiTwitter} size={iconSize} />
     </a>
-    <a href={userLinks.skype} title="Skype">
+    <a href={userLinks.skype} title="Skype" target="_blank" rel="noopener noreferrer">
       <Icon path={mdiSkype} size={iconSize} />
     </a>
-    <a href={userLinks.telegram} title="Telegram">
+    <a href={userLinks.telegram} title="Telegram" target="_blank" rel="noopener noreferrer">
       <Icon path={mdiTelegram} size={iconSize} />
     </a>
-    <a href={userLinks.email} title="Email">
+    <a href={userLinks.email} title="Email" target="_blank" rel="noopener noreferrer">
       <Icon path={mdiEmail} size={iconSize} />
     </a>
   </div>
