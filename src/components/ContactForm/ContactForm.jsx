75c47f8deb7470489e99cd590b89355602260bf1
Jim <the.jim.farrugia@gmail.com>
2019-10-01 13:31:50 +1000

    linting

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -11,23 +11,23 @@ export const ContactForm = () => (
       <div className="row">
         <div className="col s12 m6 l5">
           <div className="input-field">
-            <label htmlFor="name">Your Name:</label> {/* eslint-disable-line*/}
+            <label htmlFor="name">Your Name:</label>
             <input id="name" type="text" name="name" required />
           </div>
           <div className="input-field">
-            <label htmlFor="email">Your Email:</label> {/* eslint-disable-line*/}
+            <label htmlFor="email">Your Email:</label>
             <input id="email" type="email" name="email" required />
           </div>
           <div className="input-field">
-            <label htmlFor="subject">Subject:</label> {/* eslint-disable-line*/}
+            <label htmlFor="subject">Subject:</label>
             <input id="subject" type="text" name="subject" required />
           </div>
         </div>
         <div className="col s12 m12 l1 hide-on-med-and-down" />
         <div className="col s12 m6 l6">
           <div className="input-field">
-            <label htmlFor="message">Message:</label> {/* eslint-disable-line*/}
-            <textarea id="message" name="message" className="materialize-textarea" />
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" className="materialize-textarea" required />
           </div>
           <div className="input-field form-actions">
             <button type="submit" className="btn waves-effect waves-light">Send</button>
