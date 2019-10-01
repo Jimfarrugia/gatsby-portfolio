c4ed9efda8163bc54a0f91ad6871f3d2b1d92eab
Jim <the.jim.farrugia@gmail.com>
2019-10-02 09:32:40 +1000

    removed floating button from PostCard component

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "gatsby";
 
 import "./PostCard.scss";
 
@@ -8,13 +7,6 @@ const PostCard = ({ post }) => (
     <div className="card-image">
       <img src={post.cover} alt={post.title} />
       <span className="card-title">{post.title}</span>
-      <Link
-        to="/"
-        className="btn-floating halfway-fab waves-effect waves-light"
-      >
-        {/* link to modal id  */}
-        <i className="material-icons">image</i>
-      </Link>
     </div>
     <div className="card-content">
       <p>
