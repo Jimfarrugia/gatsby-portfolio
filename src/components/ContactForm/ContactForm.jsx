fa2d39f9a73476708a6584aca84d6a20c72d0b86
Jim <the.jim.farrugia@gmail.com>
2019-10-01 12:46:48 +1000

    ContactForm styling

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -10,36 +10,28 @@ export const ContactForm = () => (
       <input type="hidden" name="form-name" value="contact" />
       <div className="row">
         <div className="col s12 m6 l5">
-          <p>
-            <label htmlFor="name">
-              Your Name: 
-              <input id="name" type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label htmlFor="email">
-              Your Email: 
-              <input id="email" type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label htmlFor="subject">
-              Subject:
-              <input id="subject" type="text" name="subject" />
-            </label>
-          </p>
+          <div className="input-field">
+            <label htmlFor="name">Your Name:</label> {/* eslint-disable-line*/}
+            <input id="name" type="text" name="name" required />
+          </div>
+          <div className="input-field">
+            <label htmlFor="email">Your Email:</label> {/* eslint-disable-line*/}
+            <input id="email" type="email" name="email" required />
+          </div>
+          <div className="input-field">
+            <label htmlFor="subject">Subject:</label> {/* eslint-disable-line*/}
+            <input id="subject" type="text" name="subject" required />
+          </div>
         </div>
         <div className="col s12 m12 l1 hide-on-med-and-down" />
         <div className="col s12 m6 l6">
-          <p>
-            <label htmlFor="message">
-              Message: 
-              <textarea id="message" name="message" />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+          <div className="input-field">
+            <label htmlFor="message">Message:</label> {/* eslint-disable-line*/}
+            <textarea id="message" name="message" className="materialize-textarea" />
+          </div>
+          <div className="input-field form-actions">
+            <button type="submit" className="btn waves-effect waves-light">Send</button>
+          </div>
         </div>
       </div>
     </form>
