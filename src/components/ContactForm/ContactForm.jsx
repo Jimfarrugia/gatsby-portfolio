8e4ca7c62e04e0f817d8e1dd686cbcefdadcd64b
Jim <the.jim.farrugia@gmail.com>
2019-10-05 19:31:23 +1000

    ContactForm styling

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,38 +3,40 @@ import React from "react";
 import "./ContactForm.scss";
 
 export const ContactForm = () => (
-  <div id="contact-form" className="container">
-    <h3>Contact Me</h3>
-    {/* Netlify Form */}
-    <form name="contact" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
-      <div className="row">
-        <div className="col s12 m6 l5">
-          <div className="input-field">
-            <label htmlFor="name">Your Name:</label>
-            <input id="name" type="text" name="name" required />
+  <div id="contact-form-container">
+    <div id="contact-form" className="container">
+      <h3>Contact Me</h3>
+      {/* Netlify Form */}
+      <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact" />
+        <div className="row">
+          <div className="col s12 m6 l5">
+            <div className="input-field">
+              <label htmlFor="name">Your Name:</label>
+              <input id="name" type="text" name="name" required />
+            </div>
+            <div className="input-field">
+              <label htmlFor="email">Your Email:</label>
+              <input id="email" type="email" name="email" required />
+            </div>
+            <div className="input-field">
+              <label htmlFor="subject">Subject:</label>
+              <input id="subject" type="text" name="subject" required />
+            </div>
           </div>
-          <div className="input-field">
-            <label htmlFor="email">Your Email:</label>
-            <input id="email" type="email" name="email" required />
-          </div>
-          <div className="input-field">
-            <label htmlFor="subject">Subject:</label>
-            <input id="subject" type="text" name="subject" required />
+          <div className="col s12 m12 l1 hide-on-med-and-down" />
+          <div className="col s12 m6 l6">
+            <div className="input-field">
+              <label htmlFor="message">Message:</label>
+              <textarea id="message" name="message" className="materialize-textarea" required />
+            </div>
+            <div className="input-field form-actions">
+              <button type="submit" className="btn waves-effect waves-light">Send</button>
+            </div>
           </div>
         </div>
-        <div className="col s12 m12 l1 hide-on-med-and-down" />
-        <div className="col s12 m6 l6">
-          <div className="input-field">
-            <label htmlFor="message">Message:</label>
-            <textarea id="message" name="message" className="materialize-textarea" required />
-          </div>
-          <div className="input-field form-actions">
-            <button type="submit" className="btn waves-effect waves-light">Send</button>
-          </div>
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   </div>
 );
 
