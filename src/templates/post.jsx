acc1741f197d5b7141c051c41dca10ed8330765f
Jim <the.jim.farrugia@gmail.com>
2019-10-06 20:35:58 +1100

    image gallery in post template

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import Helmet from "react-helmet";
-import { graphql, Link } from "gatsby";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { graphql } from "gatsby";
 import Icon from "@mdi/react";
 import { mdiGithubCircle, mdiLaunch } from "@mdi/js";
+import Gallery from "react-photo-gallery";
 import Layout from "../layout";
 import PostTags from "../components/PostTags/PostTags";
 import SEO from "../components/SEO/SEO";
@@ -11,82 +13,112 @@ import config from "../../data/SiteConfig";
 import "./post.scss";
 import "./b16-tomorrow-dark.css";
 
-export default class PostTemplate extends React.Component {
-  renderImages = (images, altText) => {
-    return images.map((url, index) => (
-      <Link to={url}>
-        <img
-          src={url}
-          alt={`${altText} ${index+1}`}
-          width="100"
-          height="100"
-        />
-      </Link>
-    ));
+export const PostTemplate = ({ data, pageContext }) => {
+  const { slug } = pageContext;
+  const postNode = data.markdownRemark;
+  const post = postNode.frontmatter;
+  if (!post.id) {
+    post.id = slug;
   }
+  if (!post.category_id) {
+    post.category_id = config.postDefaultCategoryID;
+  }
+  const images = post.images.map(src => {
+    // get width & height values from specific indices in src string
+    const width = +src.charAt(src.length - 7);
+    const height = +src.charAt(src.length - 5);
+    return { src, width, height };
+  });
 
-  render() {
-    const { data, pageContext } = this.props;
-    const { slug } = pageContext;
-    const postNode = data.markdownRemark;
-    const post = postNode.frontmatter;
-    if (!post.id) {
-      post.id = slug;
-    }
-    if (!post.category_id) {
-      post.category_id = config.postDefaultCategoryID;
-    }
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-    return (
-      <Layout>
-        <Helmet>
-          <title>{`${post.title} | ${config.siteTitle}`}</title>
-        </Helmet>
-        <SEO postPath={slug} postNode={postNode} postSEO />
-        <div id="post" className="container">
-          <h2 className="post-title">{post.title}</h2>
-          {/* render video if it exists, else render cover */}
-          {post.video && (
-            <iframe 
-              className="video"
-              title={post.title}
-              src={post.video}
-              frameBorder="0"
-              allowFullScreen
-            />
-          ) || (
-            <img className="post-cover" src={post.cover} alt={post.title} />
-          )}
-          {(post.demo || post.github) && (
-            <div className="post-links">
-              {post.demo && (
-                <a href={post.demo} className="btn" target="_blank" rel="noopener noreferrer">
-                  <Icon path={mdiLaunch} size={0.95} />
-                  Live Demo
-                </a>
-              )}
-              {post.github && (
-                <a href={post.github} className="btn" target="_blank" rel="noopener noreferrer">
-                  <Icon path={mdiGithubCircle} size={1} />
-                  Github
-                </a>
-              )}
-            </div>
-          )}
-          <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
-          {post.images && post.images.length > 0 && (
-            <div className="post-images">
-              {this.renderImages(post.images, post.title)}
-            </div>
-          )}
-          <div className="post-tags">
-            <PostTags tags={post.tags} />
+  // eslint-disable-next-line
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+  
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>{`${post.title} | ${config.siteTitle}`}</title>
+      </Helmet>
+      <SEO postPath={slug} postNode={postNode} postSEO />
+      <div id="post" className="container">
+        <h2 className="post-title">{post.title}</h2>
+        {/* render video if it exists, else render cover */}
+        {(post.video && (
+          <iframe
+            className="video"
+            title={post.title}
+            src={post.video}
+            frameBorder="0"
+            allowFullScreen
+          />
+        )) || (
+          <img className="post-cover" src={post.cover} alt={post.title} />
+        )}
+        {(post.demo || post.github) && (
+          <div className="post-links">
+            {post.demo && (
+              <a
+                href={post.demo}
+                className="btn"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon path={mdiLaunch} size={0.95} />
+                Live Demo
+              </a>
+            )}
+            {post.github && (
+              <a
+                href={post.github}
+                className="btn"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Icon path={mdiGithubCircle} size={1} />
+                Github
+              </a>
+            )}
           </div>
+        )}
+        <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
+        {images && images.length > 1 && (
+          <div className="post-gallery">
+            <Gallery photos={images} onClick={openLightbox} />
+            <ModalGateway>
+              {viewerIsOpen ? (
+                <Modal onClose={closeLightbox}>
+                  <Carousel
+                    currentIndex={currentImage}
+                    views={images.map(x => ({
+                      ...x,
+                      srcset: x.srcSet,
+                      caption: x.title
+                    }))}
+                  />
+                </Modal>
+              ) : null}
+            </ModalGateway>
+          </div>
+        )}
+        <div className="post-tags">
+          <PostTags tags={post.tags} />
         </div>
-      </Layout>
-    );
-  }
+      </div>
+    </Layout>
+  );
 }
+
+export default PostTemplate;
 
 /* eslint no-undef: "off" */
 export const pageQuery = graphql`
