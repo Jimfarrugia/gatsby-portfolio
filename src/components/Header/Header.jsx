4c980056a0b10f2159cd9b3ed4cde8c19050a218
Jim <the.jim.farrugia@gmail.com>
2019-10-12 13:08:39 +1100

    use LoadableSidenav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 
-import Sidenav from "../Sidenav/Sidenav";
+import Sidenav from "../Sidenav/LoadableSidenav";
 
 import "./Header.scss";
 
